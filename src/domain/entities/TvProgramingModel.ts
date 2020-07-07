5eba718a0e1cb2de15cbc140f1415d9bbc4fd949
Neto Moura <netomourajs@gmail.com>
2020-07-06 21:54:40 -0300

    adicionado entidade TvProgramming

---
+++ src/domain/entities/TvProgramingModel.ts
@@ -0,0 +1,9 @@
+export interface TvProgrammingModel {
+    title: string;
+    description: string;
+    start_time: Date;
+    end_time: Date;
+    logoUrl: string;
+    imgUrl: string;
+    duracao: number;
+}
